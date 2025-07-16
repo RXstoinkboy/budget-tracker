4ddf6c42648ff25dbdff1544a484b30ab8efcc9f
RXstoinkboy <erykludwin@gmail.com>
2025-07-16 22:35:06 +0200

    workaround in metro to run supabase

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,9 @@ const config = getDefaultConfig(__dirname, {
     // [Web-only]: Enables CSS support in Metro.
     isCSSEnabled: true,
 });
+
+config.resolver.unstable_enablePackageExports = false;
+
 // add nice web support with optimizing compiler + CSS extraction
 const { withTamagui } = require('@tamagui/metro-plugin');
 
